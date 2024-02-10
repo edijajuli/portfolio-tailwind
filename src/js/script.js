c2aaf5012f43b8bb4eb296e80c8a1bbbdbe5cb48
edijajuli <edijajuli1@gmail.com>
2024-02-11 01:54:28 +0700

    update dark mode + perbaikan fitur

--- src/js/script.js
+++ src/js/script.js
@@ -11,10 +11,45 @@ humburger.addEventListener('click', function () {
 window.onscroll = function () {
     const header = document.querySelector('header')
     const fixedNav = header.offsetTop
+    const toTop = document.querySelector('#to-top')
 
     if (window.scrollY > fixedNav) {
         header.classList.add('navbar-fixed')
+        toTop.classList.remove('hidden')
+        toTop.classList.add('flex')
     } else {
         header.cclassList.remove('navbar-fixed')
+        toTop.classList.remove('flex')
+        toTop.classList.add('hidden')
     }
+}
+
+//klick diluar humburger
+window.addEventListener('click', function (e) {
+    if (e.target != humburger && e.target != navMenu) {
+        humburger.classList.remove('humburger-active')
+        navMenu.classList.add('hidden')
+    }
+})
+
+//dark mode toggle
+const darkToggle = document.querySelector('#dark-toggle')
+const html = document.querySelector('html')
+
+darkToggle.addEventListener('click', function () {
+    if (darkToggle.checked) {
+        html.classList.add('dark')
+        localStorage.theme = 'dark'
+    } else {
+        html.classList.remove('dark')
+        localStorage.theme = 'light'
+    }
+})
+
+//pindahkan posisi toggle sesuai mode 
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark')
+    darkToggle.checked = true
+} else {
+    darkToggle.checked = false
 }
